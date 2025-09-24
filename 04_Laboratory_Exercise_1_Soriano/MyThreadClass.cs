fd62502bea719091d444f1d27cd7f7fc6ac97b43
Schtemberg0147 <joseiiis013@gmail.com>
2025-09-24 16:36:49 +0800

    Add project files.

---
+++ 04_Laboratory_Exercise_1_Soriano/MyThreadClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Laboratory_Exercise_1_Soriano
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+
+        }
+    }
+}
